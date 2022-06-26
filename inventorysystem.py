6e40dd229f478590d4939c35da780586479f72c9
Subash S <76615324+smashysubash@users.noreply.github.com>
2022-06-26 16:52:53 +0530

    Add files via upload

--- inventorysystem.py
+++ inventorysystem.py
@@ -31,23 +31,6 @@ class item:
     def getitemQty(self):
         return self.__itemQty
 
-    # def getemail(self):
-    #     return self.__email
-
-    # def setname(self, value):
-    #     self.__name = value
-
-    # def settelephoneno(self, value):
-    #     self.__telephoneno = value
-
-    # def setemail(self, value):
-    #     self.__email = value
-
-    # def getpersontype(self):
-    #     return self.__persontype
-    
-    # def setpersontype(self,value):
-    #     self.__persontype=value
 
     def validateinteger(self):
             
@@ -87,10 +70,6 @@ class start:
 
             # conn.execute('''CREATE TABLE item (ItemNo INT NOT NULL, NAME  TEXT NOT NULL,Quantity int  NOT NULL,MRP INT NOT NULL,Rate INT NOT NULL);''')
 
-            #conn.execute('''CREATE TABLE professor (NAME  TEXT NOT NULL,telephoneno INT NOT NULL,email text NOT NULL,pid int,dept  text,desig text,basic real, sal real);''')
-            # conn.execute('''drop table item''')
-
-            #conn.execute('''CREATE TABLE address (addressline text NOT NULL,city  TEXT    NOT NULL,zipcode INT     NOT NULL,state         text, customerid  int);''')
             itemNo=(input("Enter item no. "))           
             itemName=input("Enter item Name ")
             itemQty = (input("Enter the quantity"))
@@ -116,10 +95,6 @@ class start:
         for n in cur.fetchall():
             print(n) 
 
-    # def viewallprofessor(self):
-    #     cur.execute("select * 1from Professor")
-    #     for n in cur.fetchall():
-    #         print(n)  
 
     def gotooptions(self):
         ch=input("Do you wish to continue(y/n)")
