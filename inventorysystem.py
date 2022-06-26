512de75dbdbd5951a7184a50cf00827d7eeab733
Smashy <rocksubash2003@gmail.com>
2022-06-26 18:58:02 +0530

    update

--- inventorysystem.py
+++ inventorysystem.py
@@ -1,6 +1,5 @@
 import sys;
-import sqlite3; 
-import re  
+import sqlite3;   
 
 
 conn=sqlite3.connect("inventory.db")
@@ -31,7 +30,6 @@ class item:
     def getitemQty(self):
         return self.__itemQty
 
-
     def validateinteger(self):
             
         if(self.__itemQty.isnumeric() and self.__itemNo.isnumeric() and self.__itemRate.isnumeric() and self.__itemMRP.isnumeric()):
@@ -39,7 +37,13 @@ class item:
         else:
             print("Enter a valid input")
             return False
-
+        
+    def validatetable(self):
+        cur.execute("select itemNo from item")    
+        for n in cur.fetchall():
+            if(n[0]==int(self.__itemNo)):
+                return False
+        return True 
 
 
 class start:
@@ -50,25 +54,28 @@ class start:
         print(" Inventory Management System")
         print("******************************")
         print("1. Add an Item")
-        print("2. Sell an item")
-        print("3. view item list")
-        print("4. Exit")
-        n=int(input("Select your choice"))
+        print("2. Update an item")
+        print("3. Delete an item")
+        print("4. view item list")
+        print("5. Exit")
+        n=int(input("Select your choice "))
         if n==1:
             self.additem()
         elif n==2:
-            self.sellitem()
+            self.updateitem()
         elif n==3:
-            self.viewitem()
+            self.deleteitem()
         elif n==4:
+            self.viewitem()
+        elif n==5:
             print("Thank You!!!")
             sys.exit
            
     
     def additem(self):
         try:
-
-            # conn.execute('''CREATE TABLE item (ItemNo INT NOT NULL, NAME  TEXT NOT NULL,Quantity int  NOT NULL,MRP INT NOT NULL,Rate INT NOT NULL);''')
+            
+            conn.execute('''CREATE TABLE if not exists item (ItemNo INT NOT NULL, NAME  TEXT NOT NULL,Quantity int  NOT NULL,MRP INT NOT NULL,Rate INT NOT NULL);''')
 
             itemNo=(input("Enter item no. "))           
             itemName=input("Enter item Name ")
@@ -77,7 +84,7 @@ class start:
             itemRate=input("Enter item Rate ")
             itemobj=item(itemNo,itemName,itemQty,itemMRP,itemRate)
 
-            if(itemobj.validateinteger()):     
+            if(itemobj.validateinteger() and itemobj.validatetable()):     
     #           # insert record in item table
                 conn.execute("""insert into item values (?,?,?,?,?)""",(itemobj.getitemno(),itemobj.getitemname(),itemobj.getitemQty(),itemobj.getitemmrp(),itemobj.getitemrate()))  
             else:
@@ -87,23 +94,56 @@ class start:
             print("Error details: ",e)
         finally:
             conn.commit()
-            
-  
-    
+        
     def viewitem(self):
-        cur.execute("select * from item")
+        cur.execute("select * from item")    
         for n in cur.fetchall():
             print(n) 
+        obj.gotooptions()
 
+
+
+    def updateitem(self):
+        try:
+            itemno=input("Enter item no. ")
+            itemqty=input("Enter the updatable quantity ")
+            if(itemqty.isnumeric() and itemno.isnumeric()):
+                conn.execute("""update item set Quantity= ? where itemNo= ? """,(itemqty,itemno) )
+            else:
+                print("Invalid Input")
+                start.gotooptions(self)
+            
+        except sqlite3.DatabaseError as e:
+            print("Error details: ",e)
+        finally:
+            conn.commit()
+
+    def deleteitem(self):
+        try:
+            itemno=input("Enter item no. ")
+            if(itemno.isnumeric()):
+                conn.execute("""delete from item where itemNo= ? """,[itemno] )
+            else:
+                print("Invalid Input")
+                start.gotooptions(self)
+                
+
+        except sqlite3.DatabaseError as e:
+            print("Error details: ",e)
+        finally:
+            conn.commit()            
 
     def gotooptions(self):
         ch=input("Do you wish to continue(y/n)")
         if(ch=='y' or ch=='Y'):
             start.initial(self)
 
-
+    # def checktable(){
+    #     if(SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}')
+    # }
 
 obj=start()
 obj.initial()
 obj.gotooptions()
+
 
